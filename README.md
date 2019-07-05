c34a0baaa5bbbcfc95409f8f02569a6e0a2eadd1
Badr <badr.eddial@gmail.com>
2019-07-05 15:17:49 +0200

    updated

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# StoryTEQ API PHP Wrapper
+# Storyteq API PHP Client
+A (incomplete) client for the Storyteq API.
+## Installation
+
+```
+composer require storyteq/storyteq-api-php
+```
+
+## Usage
+```php
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Storyteq\Client;
+
+$storyteq = new Client('v4', 'your-api-token');
+
+var_dump($storyteq->readFeedData(1));
+?>
+```
---
+++ src/Storyteq/Client.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Storyteq;
+
+use GuzzleHttp\RequestOptions;
+use function GuzzleHttp\json_decode;
+use GuzzleHttp\Client as GuzzleClient;
+
+
+class Client
+{
+    protected $token;
+    protected $version;
+    protected $base;
+
+    /**
+     * @param [type] $version [description]
+     * @param [type] $token   [description]
+     */
+    public function __construct($version, $token)
+    {
+       $this->token = $token;
+       $this->version = $version;
+       
+       $this->init();
+    }
+
+    /**
+     * @return [type] [description]
+     */
+    public function init()
+    {
+        $this->setBase();
+
+        $this->client = new GuzzleClient([
+            'headers' => [
+                'Authorization' => 'Bearer '.$this->token,
+                'Accept'        => 'application/json',
+            ],
+        ]);
+    }
+
+    /**
+     * [setBase description]
+     */
+    public function setBase()
+    {
+        if ($this->version == 'v4'){
+            $this->base = 'https://api.storyteq.com/v4/';
+        } else {
+            throw new \Exception("This version of the API is not supported");
+        }
+    }
+
+    /**
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function readFeedData($id)
+    {
+        return $this->getRequest('content/feeds/'.$id);
+    }
+
+    /**
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function readMediaPublishements($id)
+    {
+        return $this->getRequest('content/media/'.$id.'/publishments/');
+    }
+
+    /**
+     * @param  [type] $campaignId [description]
+     * @return [type]             [description]
+     */
+    public function readDv360CampaignAds($campaignId)
+    {
+        return $this->getRequest('ads/external/dv360/campaigns/'.$campaignId.'/ads');
+    }
+
+    /**
+     * @param  [type] $mediaId       [description]
+     * @param  [type] $publishmentId [description]
+     * @return [type]                [description]
+     */
+    public function readMediaPublishement($mediaId, $publishmentId)
+    {
+        return $this->getRequest('content/media/'.$mediaId.'/publishments/'.$publishmentId);
+    }
+
+    /**
+     * @param  [type] $templateId [description]
+     * @param  [type] $parameters [description]
+     * @param  [type] $webhookUrl [description]
+     * @param  [type] $batchId    [description]
+     * @return [type]             [description]
+     */
+    public function createMedia($templateId, $parameters, $webhookUrl, $batchId = null)
+    {
+        $body = [
+            'template_parameters' => $parameters,
+            'notifications'       => [
+                [
+                    'type'  => 'webhook',
+                    'route' => $webhookUrl,
+                ],
+            ],
+        ];
+
+        if ($batchId){
+            $body['batch_id'] = $batchId;
+        }
+
+        return $this->postRequest('content/templates/'.$templateId.'/media', $body);
+    }
+
+    /**
+     * @param  [type] $templateId [description]
+     * @param  [type] $name       [description]
+     * @return [type]             [description]
+     */
+    public function createBatch($templateId, $name)
+    {
+        $body = [
+            'name' => $name
+        ];
+
+        return $this->postRequest('content/templates/'.$templateId.'/batches', $body);
+    }
+
+    /**
+     * @param [type] $mediaId
+     * @param [type] $type
+     * @return void
+     */
+    public function publishMedia($mediaId, $type, $body = null)
+    {
+        return $this->postRequest('content/media/'.$mediaId.'/publish/'.$type, $body);
+    }
+
+    /**
+     * @param [type] $endpoint
+     * @param [type] $data
+     * @return void
+     */
+    public function postRequest($endpoint, $data = null)
+    {
+        $response = json_decode(
+            $this->client->post($this->base.$endpoint, [
+                RequestOptions::JSON => $data,
+            ])
+            ->getBody()
+            ->getContents()
+        , true);
+
+        return $response;
+    }
+
+    /**
+     * @param [type] $endpoint
+     * @param [type] $data
+     * @return void
+     */
+    public function putRequest($endpoint, $data)
+    {
+        $response = json_decode(
+            $this->client->put($this->base.$endpoint, [
+                RequestOptions::JSON => $data,
+            ])
+            ->getBody()
+            ->getContents()
+        , true);
+
+        return $response;
+    }
+
+    /**
+     * @param [type] $endpoint
+     * @param bool   $decode
+     * @return void
+     */
+    public function getRequest($endpoint, $decode = true)
+    {
+        $response = $this->client->get($this->base.$endpoint)
+            ->getBody()
+            ->getContents();
+
+        if ($decode) {
+            return json_decode($response, true);
+        }
+
+        return $response;
+    }
+}
